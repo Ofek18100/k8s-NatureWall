e8be4666543712d5dd2b6722815f1935fe63603f
Guy Shalgi <guysh@shalgi-laptop>
2023-12-13 17:57:00 +0100

    work with styles

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # k8s-NatureWall
 developing website for nature wall using kubernetes
+
+COMMON MISTAKES
+if kubectl is not working - make sure the env var KUBECONFIG is setted to the civo-config file
--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"hello" : "world"}
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context)
+
